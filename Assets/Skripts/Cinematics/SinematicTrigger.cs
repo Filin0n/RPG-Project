1db692e4534c42fbc089fd96ce434a4cd2f0b667
Filin <rustam.tkachenko.00@mail.ru>
2022-06-25 20:31:51 +0600

    64. Запуск кат-сцены Cinemachine

---
+++ Assets/Skripts/Cinematics/SinematicTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class SinematicTrigger : MonoBehaviour
+    {
+        private bool _alredyTriggered = false;
+
+        private void OnTriggerEnter(Collider collider) 
+        {
+            if (collider.tag == "Player" && !_alredyTriggered)
+            {
+                _alredyTriggered = true;
+                GetComponent<PlayableDirector>().Play();
+            }
+        }
+    }
+}
