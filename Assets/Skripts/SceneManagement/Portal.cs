9af84d94f81daa1f6ce45e118604dcfa74727e4a
Filin <rustam.tkachenko.00@mail.ru>
2022-06-26 13:51:55 +0600

    69. Порталы для загрузки сцен

---
+++ Assets/Skripts/SceneManagement/Portal.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] private int _sceneToLoad = -1;
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            if (collider.tag == "Player")
+            {
+                SceneManager.LoadScene(_sceneToLoad);
+            }
+        }
+    }
+}
