b2097ee4d41eae44e7152fe44698b922304526b4
Filin <rustam.tkachenko.00@mail.ru>
2022-06-27 17:38:24 +0600

    73. Ссылки на перекрестные сцены

--- Assets/Skripts/SceneManagement/Portal.cs
+++ Assets/Skripts/SceneManagement/Portal.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        
+
         [SerializeField] private int _sceneToLoad = -1;
+        [SerializeField] private Transform spawnPoint;
 
         private void OnTriggerEnter(Collider collider)
         {
@@ -21,8 +25,28 @@ namespace RPG.SceneManagement
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(_sceneToLoad);
 
-            print("Scene Loaded");
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
             Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            Transform player = GameObject.FindWithTag("Player").transform;
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+
+                return portal;
+            }
+            return null;
         }
     }
 }
